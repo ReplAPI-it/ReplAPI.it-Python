fca2d1ceca555dd463a6dc792233f6cb0309319c
Dark Boi <>
2021-05-27 16:47:11 +0000

    Put it through black, and sort, and flake8

--- replapi_it/core.py
+++ replapi_it/core.py
@@ -3,6 +3,7 @@ from user import _User
 
 defaultInitVars = {"username": None}
 
+
 class ReplAPI(object):
   """Base class for all ReplAPI-it features.
     Everything should be accessed through this class.
--- replapi_it/user.py
+++ replapi_it/user.py
@@ -1,36 +1,48 @@
 # -*- coding: utf-8 -*-
+import json
+
 import requests
-import json 
+
 getCyclesQuery = """
-	karma
+  karma
 """
 headers = {
-  'X-Requested-With': 'ReplAPI-IT Team',
-  'Referrer': 'https://staging.replit.com/'
+  "X-Requested-With": "ReplAPI-IT Team",
+  "Referrer": "https://staging.replit.com/",
 }
-url = 'https://staging.replit.com/graphql'
+url = "https://staging.replit.com/graphql"
+
+
 class _User(object):
-  
   def __init__(self, username: str):
     """Get user data."""
     self.username = username
+
   def userGraphQLDataFull(self) -> dict:
     # example = {"karma": 102}
     # return example
     pass
+
   def getCycles(self) -> int:
     # Make body
-    body = {'query': 'query UserData { userByUsername(username: "' + self.username + '") { ' + getCyclesQuery + ' } }'}
+    body = {
+      "query": 'query UserData { userByUsername(username: "'
+      + self.username
+      + '") { '
+      + getCyclesQuery
+      + " } }"
+    }
     # Get Data from graphql
-    self.res = json.loads( requests.post(url, body,headers=headers).text)
+    self.res = json.loads(requests.post(url, body, headers=headers).text)
     returnData = self.res
     # Check if user exists
     f = returnData
-    f = f['data']['userByUsername']
-    if (f == None):
+    f = f["data"]["userByUsername"]
+    if f is None:
       # Error if user doesn't exist
-      print("We couldn't fine the user that you were looking for! The username we revieved was: " + self.username)
+      print("We couldn't fine the user that you were looking for! ")
+      print("The username we revieved was: " + self.username)
       exit(0)
-    del(f) # Delete f because we don't need it
-    returnData = returnData['data']['userByUsername']['karma'] # Make return data
-    return returnData # Return it!
+    del f  # Delete f because we don't need it
+    returnData = returnData["data"]["userByUsername"]["karma"]  # Make data
+    return returnData  # Return it!
